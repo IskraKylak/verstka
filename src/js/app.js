87635561b4633a605bc57b6ea5ce440ab4fd15e2
Vlad Family <vladuslav.belushev@ribs.studio>
2023-04-15 22:51:00 +0300

    first commit

---
+++ src/js/app.js
@@ -0,0 +1,17 @@
+// import "./modules/jquery-3.6.0.min.js"
+// import "./modules/slick.js"
+// import "./modules/select2.js"
+// import "./modules/jquery.arcticmodal.js"
+// import "./modules/jquery.validate.js"
+// import "./modules/jquery.fancybox.js"
+// import "./modules/jquery.maskedinput.js"
+// import "./modules/jquery-ui.js"
+
+
+
+
+
+
+
+
+
